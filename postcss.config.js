c2254ea2942017cd6eca91b1fe39dba077d1eb12
nickbastos <nickbastos4@gmail.com>
2020-08-05 18:20:01 -0500

    4

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === "production"
-      ? {
-          "@fullhuman/postcss-purgecss": {
-            content: ["./pages/[slug].jsx", "./pages/index.js"],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : {}),
-  },
+  plugins: ["tailwindcss", "postcss-preset-env"],
 };
